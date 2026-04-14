07d628edc56d25eff84d3d92ac82c3bed7412d33
Andrej Silin <citrulo@gmail.com>
2026-04-14 17:02:13 +0200

    Parameters change.

--- lib/configure-nft.sh
+++ lib/configure-nft.sh
@@ -6,7 +6,7 @@ VM_NET="${VM_NET:-15.10.0.0/24}"
 
 sudo nft list table ip qemu_nat >/dev/null 2>&1 || sudo nft add table ip qemu_nat
 sudo nft list chain ip qemu_nat postrouting >/dev/null 2>&1 || \
-  sudo nft add chain ip qemu_nat postrouting '{ type nat hook postrouting priority srcnat; }'
+sudo nft add chain ip qemu_nat postrouting '{ type nat hook postrouting priority srcnat; }'
 
 # Idempotent rule add (only add if missing)
 if ! sudo nft list chain ip qemu_nat postrouting | grep -q "oifname \"$WAN\".*ip saddr $VM_NET.*masquerade"; then
